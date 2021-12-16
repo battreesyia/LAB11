3cfddf399fb323c5ce4e198b5bc1cc9b7b078912
battreesyia <92843785+battreesyia@users.noreply.github.com>
2021-12-17 04:32:27 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,10 @@
 LAB11
-
 // This program is used to calculate BMI
-
 #include <iostream>
+
+void readData(double studWeight[], double studHeight[]);
+void calBMI(double studWeight[], double studHeight[], double studBMI[]);
+void display(double studBMI[]);
 
 int main()
 {
@@ -10,20 +12,36 @@ int main()
 	double studHeight[3];
 	double studBMI[3];
 
-	//enter input
+	//function call for input
+	readData(studWeight, studHeight);
+	//calculate BMI
+	calBMI(studWeight, studHeight, studBMI);
+	//display BMI
+	display(studBMI);
+}
+void readData(double studWeight[], double studHeight[])
+{
 	for (int i = 0; i < 3; i++)
 	{
 		std::cout << " Enter students weight in kg : ";
 		std::cin >> studWeight[i];
 		std::cout << "Enter students height in meters : ";
 		std::cin >> studHeight[i];
-		std::cout << "\n\n";
+		std::cout << "\n";
 	}
-
-	//calculate BMI
+}
+void calBMI(double studWeight[], double studHeight[], double studBMI[])
+{
 	for (int i = 0; i < 3; i++)
 	{
 		studBMI[i] = studWeight[i] / (studHeight[i] * studHeight[i]);
+	}
+}
+void display(double studBMI[])
+{
+	for (int i = 0; i < 3; i++)
+	{
 		std::cout << i << "    " << studBMI[i] << "\n";
 	}
 }
+
